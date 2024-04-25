e719d414f037011c6ea6ca645376183a62692497
shivamanand8969 <134687947+shivamanand8969@users.noreply.github.com>
2024-04-25 12:31:45 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data,setData]=useState([]);
+  const [page,setPage]=useState(4);
+   const fetchData=async ()=>{
+         let res=await fetch('https://dummyjson.com/products?limit=100');
+             res=await res.json();
+              setData(res.products);
+   }
+   useEffect(()=>{
+    fetchData();
+   },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+   <div className='mainbox'>
+     <div className='innerBox'>
+     {
+        data.slice((page*10)-10,page*10).map((value)=>(
+          <>
+          <div className='product'>
+              <img src={value.thumbnail} className='productimage'/>
+              <div className='titlebox'>
+                <h2>{value.title}</h2>
+              </div>
+          </div>
+          </>
+        ))
+       }
+     </div>
+     <div className='pagelist'>
+      <div className='num' onClick={()=>setPage(page-1)} style={{display:(page==1)?"none":""}}>◀</div>
+     {
+       [... Array(data.length/10)].map((_,index)=>{
+         return <div className='num' style={{backgroundColor:(index+1)==page ?"rgb(1, 31, 1)":""}} onClick={()=>setPage(index+1)}>{index+1}</div>
+        })
+      }
+      <div className='num' onClick={()=>setPage(page+1)} style={{display:(data.length/10==page ? "none":"")}}>▶</div>
+      
+     </div>
+   </div>
+ </>
   );
 }
 
